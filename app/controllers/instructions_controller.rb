608be8491eb7160acbcced17a09ba4ef8937b9fc
Andy Hull <andyhull3@gmail.com>
2013-05-02 15:57:36 -0700

    adding cors support

--- app/controllers/instructions_controller.rb
+++ app/controllers/instructions_controller.rb
@@ -18,8 +18,7 @@ class InstructionsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render :json => {:instruction => @instruction, :steps => @instruction.steps.each_with_index, :callback => params[:callback]}}
-      format.js { render :json => {:instruction => @instruction, :steps => @instruction.steps.each_with_index, :callback => params[:callback]}}
+      format.json { render :json => {:instruction => @instruction, :steps => @instruction.steps.each_with_index}}
     end
   end
 
